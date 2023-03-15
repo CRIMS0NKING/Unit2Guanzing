c172798a2609999c8c2d9c6ef674e86da8ab50c0
CRIMS0NKING <116672891+CRIMS0NKING@users.noreply.github.com>
2023-03-15 11:42:54 -0700

    update
    fixed problems and added spawn manager

--- Unit2GameplayGuanzing/Assets/Scripts/SpawnManager.cs
+++ Unit2GameplayGuanzing/Assets/Scripts/SpawnManager.cs
@@ -6,6 +6,10 @@ public class SpawnManager : MonoBehaviour
 {
    
     public GameObject[] animalPrefabs;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +21,15 @@ public class SpawnManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
-            Instantiate(animalPrefabs[0], new Vector3(0, 0, 20), animalPrefabs[0].transform.rotation);
+            SpawnRandomAnimal();
         }
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
